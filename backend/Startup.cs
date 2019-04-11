63da09150ac73b08d250a6e8896ddc82e784a1d7
Will Jones <will.jones@silverlinesolutions.com>
2019-04-11 09:27:00 -0400

    Update ASP.net from 2.0.5 to 2.1

--- backend/Startup.cs
+++ backend/Startup.cs
@@ -1,9 +1,10 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -15,7 +16,14 @@ namespace backend
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+              // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+              options.CheckConsentNeeded = context => true;
+              options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+            services.AddMvc()
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_1); ;
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,10 +33,16 @@ namespace backend
             {
                 app.UseDeveloperExceptionPage();
             }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                app.UseHsts();
+            }
 
-            app.UseDefaultFiles();
+            app.UseHttpsRedirection();
             app.UseStaticFiles();
-
+            app.UseCookiePolicy();
+            app.UseDefaultFiles();
             app.UseMvc();
         }
     }
