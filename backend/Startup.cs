0497db30484d7de7b3b6d0d0372b28bf46faf77a
Will Jones <will.jones@silverlinesolutions.com>
2019-04-12 09:22:10 -0400

    Update .NET Core from 2.1 to 2.2

--- backend/Startup.cs
+++ backend/Startup.cs
@@ -22,9 +22,10 @@ namespace backend
               options.CheckConsentNeeded = context => true;
               options.MinimumSameSitePolicy = SameSiteMode.None;
             });
+            
             services.AddMvc()
-                .SetCompatibilityVersion(CompatibilityVersion.Version_2_1); ;
-        }
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+    }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
